8ac94c139f4033454c27f581939ef45cadd82e5c
Philip White <phwhite89@gmail.com>
2022-08-09 09:16:39 +0100

    updated testing classes examples.

--- spec/counter_spec.rb
+++ spec/counter_spec.rb
@@ -2,10 +2,25 @@ require "counter"
 
 # We use the class name here rather than a string
 RSpec.describe Counter do
-  it "counts to a given number" do
+  it "initially reports a count of zero." do
+    counter = Counter.new
+    result = counter.report
+    expect(result).to eq "Counted to 0 so far."
+  end
+
+  it "reports a count of added values with one addition." do
     counter = Counter.new
     counter.add(10)
     result = counter.report
     expect(result).to eq "Counted to 10 so far."
+  end
+
+  it "reports a count of all added values." do
+    counter = Counter.new
+    counter.add(10)
+    counter.add(20)
+    counter.add(5)
+    result = counter.report
+    expect(result).to eq "Counted to 35 so far."
   end
 end
--- spec/string_builder_spec.rb
+++ spec/string_builder_spec.rb
@@ -1,7 +1,22 @@
 require "string_builder"
 
 RSpec.describe StringBuilder do
-  it "return a string as the output." do
+  context "initially"
+  it "initially returns an empty string as output." do
+    string_builder = StringBuilder.new
+    result = string_builder.output
+    expect(result).to eq ""
+  end
+
+  it "initially returns length of 0 due to empty string." do
+    string_builder = StringBuilder.new
+    result = string_builder.size
+    expect(result).to eq 0
+  end
+
+  # context sets up sub-group
+  context "given one addition of a string"
+  it "return string as output." do
     string_builder = StringBuilder.new
     string_builder.add("metro")
     result = string_builder.output
