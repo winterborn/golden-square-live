45b324f4c8ce5a45da0964e5d86602c87c6f09a2
Philip White <phwhite89@gmail.com>
2022-08-16 09:43:16 +0100

    updated spec.

--- multi-class/spec/todo_list_spec.rb
+++ multi-class/spec/todo_list_spec.rb
@@ -1,1 +1,10 @@
 require "todo_list"
+
+RSpec.describe TodoList do
+  it "returns empty list when todo is empty" do
+    todo = Todo.new("")
+    todo_list = TodoList.new
+    todo_list.add(todo)
+    expect(todo_list.incomplete).to eq [""]
+  end
+end
