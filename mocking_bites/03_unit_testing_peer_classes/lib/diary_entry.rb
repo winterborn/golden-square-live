01533497a768c43d40b78010e18b2bd297486794
Philip White <phwhite89@gmail.com>
2022-08-17 14:14:42 +0100

    unit testing peer classes set up.

---
+++ mocking_bites/03_unit_testing_peer_classes/lib/diary_entry.rb
@@ -0,0 +1,12 @@
+class DiaryEntry
+  def initialize(title, contents) # title, contents are both strings
+  end
+
+  def title
+    # Returns the title as a string
+  end
+
+  def contents
+    # Returns the contents as a string
+  end
+end
---
+++ mocking_bites/03_unit_testing_peer_classes/lib/diary_reader.rb
@@ -0,0 +1,8 @@
+class DiaryEntryFormatter
+  def initialize(diary_entry) # diary_entry is an instance of DiaryEntry
+  end
+
+  def format
+    # Returns a nicely formatted diary entry
+  end
+end
